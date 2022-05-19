1ec23436d9f84a62c9d92f69e3d96385918637d6
saner <73155590+kinoshisocial@users.noreply.github.com>
2022-05-19 19:30:18 +0300

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -4,11 +4,20 @@
 	let info = {
 		name: "Saner | Kinoshi",
 		description: "I'm a web developer",
-		socialmedia: {
-			instagram: "https://www.instagram.com/kinoshisocial/",
-			github: "https://github.com/kinoshisocial",
-			discord: "https://discord.com/users/913076582694944839"
-		}
+		socialmedia: [
+			{
+				key: "İnstagram",
+				value: "https://www.instagram.com/kinoshisocial/"
+			},
+			{
+				key: "Github",
+				value: "https://github.com/kinoshisocial"
+			},
+			{
+				key: "Discord",
+				value: "https://discord.com/users/913076582694944839"
+			}
+		]
 	}
 
 </script>
