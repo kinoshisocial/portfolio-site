419aba7bdc3484beac5bc21912ca91e14c82fd45
saner <73155590+kinoshisocial@users.noreply.github.com>
2022-05-19 17:15:10 +0300

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Page from "./Page.svelte";
+
+	let info = {
+		name: "Saner | Kinoshi",
+		description: "I'm a web developer",
+		socialmedia: {
+			instagram: "https://www.instagram.com/kinoshisocial/",
+			github: "https://github.com/kinoshisocial",
+			discord: "https://discord.com/users/913076582694944839"
+		}
+	}
+
+</script>
+
+<Page user={info} />
---
+++ src/Page.svelte
@@ -0,0 +1,54 @@
+<script>
+	export let user;
+</script>
+
+<div class="main">
+        <h1>{user.name}</h1>
+	    <p>{user.description}</p>
+    <div class="socialmedia">
+        <span><a class="instagram" target="_blank" href="{user.socialmedia.instagram}">İnstagram</a></span>
+        <span><a class="github" target="_blank" href="{user.socialmedia.github}">GitHub</a></span>
+        <span><a class="discord" target="_blank" href="{user.socialmedia.discord}">Discord</a></span>
+    </div>
+</div>
+
+<style>
+	.main {
+        display: flex !important;
+		text-align: center;
+		padding-top: 25em;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+	}
+
+    a {
+        font-size: 20px; 
+        list-style: none;
+        text-decoration: none;
+        cursor: pointer;
+        transition: font-size 550ms ease, color 550ms ease;
+        background-color: #5865F2;
+        color: #fff;
+        padding-top: 5px;
+        padding-bottom: 5px;
+        padding-left: 10px;
+        padding-right: 10px;
+        border-radius: 50px;
+    }
+
+    .socialmedia {
+        padding-top: 25px;
+    }
+
+    span {
+        padding-right: 10px;
+    }
+
+    a:hover {
+        font-size: 30px;   
+        color: #5865F2;
+        background-color: #fff;
+    }
+
+</style>
